34114fb4001747caf08591d7ecf8605285a39584
Samtou <christophe1999da@gmail.com>
2020-06-19 10:30:40 +0000

    Ajoutez des fichiers projet.

---
+++ myPass/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Walkthrough.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace myPass
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new WalkthroughView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
